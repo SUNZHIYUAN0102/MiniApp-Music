1843cd4a6cb3ce139feb9d76541ffb394022638a
SUNZHIYUAN0102 <zhiyuansun703@gmail.com>
2022-07-11 01:01:47 +0700

    it is a music miniapp

---
+++ pages/index/index.js
@@ -0,0 +1,74 @@
+// pages/index/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        msg: '监听页面加载'
+    },
+
+    handleChild(){
+        console.log('child');
+    },
+
+    handleParent(){
+        console.log('parent');
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+        setTimeout(() => {
+            this.setData({
+                msg: 'Moew'
+            })
+        }, 1000)
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
